37973e3c0a7d32575e87ad0ec8d7aff4882373c4
wzt <1586007080@qq.com>
2023-04-11 21:34:13 +0800

    更新

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,7 +1,6 @@
 import Link from "@/components/link";
 import '../styles/Index.module.css'
-
-import Box from '@mui/material/Box';
+import { Grid,Button, Box } from "@mui/material";
 
 
 export default function Home() {
@@ -10,21 +9,20 @@ export default function Home() {
 
   return (
     <>
-      <Box
-      sx={{
-        width: 1450,
-        height: 30,
-        backgroundColor:'purple',
-      }}></Box>
-      
-      <Box
-      sx={{
-        width: 1450,
-        height: 400,
-        backgroundColor: 'black',
-      }}>
-        <div></div>
-      </Box>    
+<Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+  <Box gridColumn="span 8">
+    <Item>xs=8</Item>
+  </Box>
+  <Box gridColumn="span 4">
+    <Item>xs=4</Item>
+  </Box>
+  <Box gridColumn="span 4">
+    <Item>xs=4</Item>
+  </Box>
+  <Box gridColumn="span 8">
+    <Item>xs=8</Item>
+  </Box>
+</Box>
       <Link />
     </>
 
